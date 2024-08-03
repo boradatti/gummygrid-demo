eebd2ae39fd7b0486c057c4c0e915fac9cbb24c1
Boris Adaev <mr.bobod@mail.ru>
2024-08-03 15:07:20 +0300

    fixed import aliases + added svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    react(),
+    tsconfigPaths({
+      configNames: ["tsconfig.app.json"],
+    }),
+    svgr(),
+  ],
 });
