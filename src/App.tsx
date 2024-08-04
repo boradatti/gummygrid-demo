9976ddb05a2dff6757dd5d9e739db6615938d26b
Boris Adaev <mr.bobod@mail.ru>
2024-08-04 09:35:11 +0300

    avatar + input

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,17 @@ import {
   DropdownMenuTrigger,
 } from "./components/ui/dropdown-menu";
 import clsx from "clsx";
+import GummyGrid from "gummygrid";
+import { Input } from "./components/ui/input";
+import { useState } from "react";
 
 function App() {
   const { theme, themeChoice, setThemeChoice } = useTheme();
+  const [usernameInput, setUsernameInput] = useState("");
+  const gg = new GummyGrid({
+    grid: { ensureFill: { leftRight: true, topBottom: true } },
+  });
+  const svg = gg.buildFrom(usernameInput);
 
   let ThemeIcon;
   switch (theme) {
@@ -28,11 +36,11 @@ function App() {
   }
 
   return (
-    <div className="grid h-[100svh] place-items-center font-mono">
-      <div className="flex flex-col gap-3">
+    <div className="h-[100svh] font-mono md:grid md:place-items-center">
+      <div className="mx-auto mt-4 flex w-[95svw] max-w-[50rem] flex-col gap-3">
         <header className="flex items-center gap-5">
-          <div className="flex items-baseline gap-3">
-            <div className="h-3 w-3 bg-foreground"></div>
+          <div className="ml-3 flex items-center gap-3 md:ml-0">
+            <div className="h-2.5 w-2.5 bg-foreground"></div>
             <h1 className="text-2xl font-medium">GummyGrid</h1>
           </div>
 
@@ -42,7 +50,7 @@ function App() {
             <a
               href="https://github.com/boradatti/gummygrid"
               target="_blank"
-              className={`flex gap-3 ${buttonVariants({ variant: "destructive" })}`}
+              className={`flex gap-3 ${buttonVariants({ variant: "outline" })}`}
             >
               <GithubIcon className="w-5" />
               view repo
@@ -60,7 +68,7 @@ function App() {
                     "bg-neutral-100 bg-opacity-80": themeChoice == "dark",
                   })}
                 >
-                  <span>dark</span>
+                  dark
                   <MoonIcon className="w-5" />
                 </DropdownMenuItem>
                 <DropdownMenuItem
@@ -69,7 +77,7 @@ function App() {
                     "bg-neutral-100 bg-opacity-80": themeChoice == "light",
                   })}
                 >
-                  <span>light</span>
+                  light
                   <SunIcon className="w-5" />
                 </DropdownMenuItem>
                 <DropdownMenuItem
@@ -78,7 +86,7 @@ function App() {
                     "bg-neutral-100 bg-opacity-80": themeChoice == "system",
                   })}
                 >
-                  <span>system</span>
+                  system
                   <MonitorIcon className="w-5" />
                 </DropdownMenuItem>
               </DropdownMenuContent>
@@ -86,7 +94,23 @@ function App() {
           </div>
         </header>
 
-        <div className="h-1 w-[80vw] max-w-[40rem]"></div>
+        <div>
+          <div className="flex w-fit flex-col gap-3">
+            <div className="border-[3px] border-solid">
+              <img
+                className="w-56"
+                src={svg.toURLEncodedString({ withPrefix: true })}
+                alt="generated avatar"
+              />
+            </div>
+
+            <Input
+              placeholder="enter username..."
+              value={usernameInput}
+              onChange={(e) => setUsernameInput(e.currentTarget.value)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
