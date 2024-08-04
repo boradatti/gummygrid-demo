32e392b24366ece5636b96da63db2248675d4ee0
Boris Adaev <mr.bobod@mail.ru>
2024-08-04 17:40:15 +0300

    refactored header

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,25 @@
-import MonitorIcon from "@/assets/icons/monitor.svg?react";
 import PlusIcon from "@/assets/icons/plus.svg?react";
 import LinkIcon from "@/assets/icons/link.svg?react";
 import UnlinkIcon from "@/assets/icons/unlink.svg?react";
-import GithubIcon from "@/assets/icons/github.svg?react";
-import MoonIcon from "@/assets/icons/moon.svg?react";
-import SunIcon from "@/assets/icons/sun.svg?react";
-import { Button, buttonVariants } from "@/components/ui/button";
+
 import { useTheme } from "./contexts/theme";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./components/ui/dropdown-menu";
-import clsx from "clsx";
+
 import { Input } from "./components/ui/input";
 import { useState } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./components/ui/tooltip";
+
 import { Checkbox } from "./components/ui/checkbox";
 import { useGummyGrid } from "./contexts/gummygrid/provider";
+import { Header } from "./components/layout/header";
 
 function App() {
-  const { theme, themeChoice, setThemeChoice } = useTheme();
   const [usernameInput, setUsernameInput] = useState("");
   const { gg, ggReconfig } = useGummyGrid();
   const svg = gg.buildFrom(usernameInput);
 
-  let ThemeIcon;
-  switch (theme) {
-    case "dark": {
-      ThemeIcon = MoonIcon;
-      break;
-    }
-    case "light": {
-      ThemeIcon = SunIcon;
-      break;
-    }
-  }
-
   return (
     <div className="font-mono md:grid md:h-[100svh] md:place-items-center">
       <div className="mx-auto mt-4 flex w-[95svw] max-w-[60rem] flex-col gap-3">
-        <header className="flex items-center gap-5">
-          <div className="ml-1 flex items-center gap-3 md:ml-0">
-            <div className="h-2.5 w-2.5 bg-foreground"></div>
-            <h1 className="text-2xl font-medium">GummyGrid</h1>
-          </div>
-
-          <div className="h-[0.5px] flex-grow bg-neutral-200"></div>
-
-          <div className="flex gap-2">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <a
-                    href="https://github.com/boradatti/gummygrid"
-                    target="_blank"
-                    className={`flex gap-3 ${buttonVariants({ variant: "outline" })}`}
-                  >
-                    <GithubIcon className="w-5" />
-                    view repo
-                  </a>
-                </TooltipTrigger>
-                <TooltipContent className="mb-1">
-                  more customizable in code
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant={"outline"} size={"icon"}>
-                  <ThemeIcon className="w-5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="mt-1" align="end">
-                <DropdownMenuItem
-                  onClick={() => setThemeChoice("dark")}
-                  className={clsx("flex justify-between", {
-                    "bg-neutral-100 bg-opacity-80": themeChoice == "dark",
-                  })}
-                >
-                  dark
-                  <MoonIcon className="w-5" />
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => setThemeChoice("light")}
-                  className={clsx("flex justify-between", {
-                    "bg-neutral-100 bg-opacity-80": themeChoice == "light",
-                  })}
-                >
-                  light
-                  <SunIcon className="w-5" />
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => setThemeChoice("system")}
-                  className={clsx("flex justify-between", {
-                    "bg-neutral-100 bg-opacity-80": themeChoice == "system",
-                  })}
-                >
-                  system
-                  <MonitorIcon className="w-5" />
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </header>
+        <Header />
 
         <div className="flex gap-7">
           <div className="flex w-fit flex-col gap-3">
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,102 @@
+import MoonIcon from "@/assets/icons/moon.svg?react";
+import SunIcon from "@/assets/icons/sun.svg?react";
+import MonitorIcon from "@/assets/icons/monitor.svg?react";
+import GithubIcon from "@/assets/icons/github.svg?react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import clsx from "clsx";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useTheme } from "@/contexts/theme";
+
+export const Header = () => {
+  const { theme, themeChoice, setThemeChoice } = useTheme();
+
+  let ThemeIcon;
+  switch (theme) {
+    case "dark": {
+      ThemeIcon = MoonIcon;
+      break;
+    }
+    case "light": {
+      ThemeIcon = SunIcon;
+      break;
+    }
+  }
+
+  return (
+    <header className="flex items-center gap-5">
+      <div className="ml-1 flex items-center gap-3 md:ml-0">
+        <div className="h-2.5 w-2.5 bg-foreground"></div>
+        <h1 className="text-2xl font-medium">GummyGrid</h1>
+      </div>
+
+      <div className="h-[0.5px] flex-grow bg-neutral-200"></div>
+
+      <div className="flex gap-2">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href="https://github.com/boradatti/gummygrid"
+                target="_blank"
+                className={`flex gap-3 ${buttonVariants({ variant: "outline" })}`}
+              >
+                <GithubIcon className="w-5" />
+                view repo
+              </a>
+            </TooltipTrigger>
+            <TooltipContent className="mb-1">
+              more customizable in code
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"outline"} size={"icon"}>
+              <ThemeIcon className="w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mt-1" align="end">
+            <DropdownMenuItem
+              onClick={() => setThemeChoice("dark")}
+              className={clsx("flex justify-between", {
+                "bg-neutral-100 bg-opacity-80": themeChoice == "dark",
+              })}
+            >
+              dark
+              <MoonIcon className="w-5" />
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setThemeChoice("light")}
+              className={clsx("flex justify-between", {
+                "bg-neutral-100 bg-opacity-80": themeChoice == "light",
+              })}
+            >
+              light
+              <SunIcon className="w-5" />
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setThemeChoice("system")}
+              className={clsx("flex justify-between", {
+                "bg-neutral-100 bg-opacity-80": themeChoice == "system",
+              })}
+            >
+              system
+              <MonitorIcon className="w-5" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+};
